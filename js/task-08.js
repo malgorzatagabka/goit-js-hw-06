259d4a331da09641e2f7a675a1c9f76978b2c6c3
Małgorzata Gąbka <malgorzata.dycha@gmail.com>
2022-07-15 21:55:08 +0200

    tasks
    8,9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  
+    const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Proszę uzupełnić wszystkie pola!");
+  }
+
+    const result = { Email: email.value, Password: password.value };
+    
+  console.log(result);
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const setColor = document.querySelector(".color");
+const setBg = document.querySelector(".change-color");
+
+setBg.addEventListener("click", (event) => {
+  body.style.background = getRandomHexColor();
+  setColor.textContent = getRandomHexColor();
+});
