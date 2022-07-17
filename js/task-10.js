4569a966daa12667a1ad261e0cc313f5aff412aa
Małgorzata Gąbka <malgorzata.dycha@gmail.com>
2022-07-17 20:16:37 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
+const create = document.querySelector("button[data-create]");
+const destroy = document.querySelector("button[data-destroy]");
+const boxes = document.querySelector("#boxes");
+const input = document.querySelector("input");
+
+create.addEventListener("click", () => createBoxes(amount));
+input.addEventListener("input", (e) => {
+  amount = e.target.value;
+});
+destroy.addEventListener("click", () => destroyBoxes());
+
+let amount = 0;
+
+function createBoxes(amount) {
+  let boxSize = 30;
+
+  for (let i = 0; i < amount; i += 1, boxSize += 10) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.background = getRandomHexColor();
+    box.style.height = `${boxSize}px`;
+    box.style.width = `${boxSize}px`;
+
+    boxes.append(box);
+  }
+}
+
+const destroyBoxes = () => {
+  boxes.textContent = "";
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
